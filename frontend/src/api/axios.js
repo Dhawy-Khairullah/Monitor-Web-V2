4c2a553cbf3179f4d981828ee1a59492721ac917
Dhawy-Khairullah <dhawykhairullah@gmail.com>
2025-06-20 16:29:32 +0700

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://http://103.127.98.204/",
 });
 
 instance.interceptors.request.use(config => {
